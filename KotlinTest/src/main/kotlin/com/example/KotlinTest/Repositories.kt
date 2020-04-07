e73ad2d103768dd2fdb3de2d8c769c1fd5ef6ba9
Chrisfufu <zuofu@ualberta.ca>
2020-04-07 16:02:40 -0600

    add comments for repositories

--- KotlinTest/src/main/kotlin/com/example/KotlinTest/Repositories.kt
+++ KotlinTest/src/main/kotlin/com/example/KotlinTest/Repositories.kt
@@ -3,19 +3,32 @@ package com.example.KotlinTest
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
-
+/*
+* it has two methods,
+* 1. find by sessionId
+* 2. find by Id, this is not in use.
+* return: list of sessions
+*/
 @Repository
 interface SessionRepository : CrudRepository<Session, Long> {
     fun findBySessionId(sessionId: String?): List<Session>
     fun findById(id: Long?): List<Session>
 }
 
+/*
+* it has two methods,
+* 1. find by type, this is not in use.
+* 2. find by Id.
+* return: list of Actions
+*/
 @Repository
 interface ActionsRepository : CrudRepository<Actions, Long> {
     fun findByType(type: String?): List<Actions>
     fun findById(id: Long?): List<Actions>
 }
-
+/*
+* it does not have any method because property is the third part
+* of the sample log. It does not have child.
+*/
 @Repository
-interface PropertiesRepository : CrudRepository<Properties, Long> {
-}
+interface PropertiesRepository : CrudRepository<Properties, Long> {}
